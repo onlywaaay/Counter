fb93f07115f5ec84373554cc6799a8550090f888
Artush <artushikmeloyan@icloud.com>
2023-09-30 20:49:58 +0600

    Добавлен счетчик, отображающий количество касаний кнопки "Считать"

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var textCounter: UILabel!
+    @IBAction func buttonCounter(_ sender: Any) {
+        count += 1
+        textCounter.text = String(count)
+    }
 }
 
